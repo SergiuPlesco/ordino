74c30eebe369e0bee6c3c963e6881c258ab51718
sergiuplesco <plescosergiu@gmail.com>
2023-02-27 08:04:03 +0200

    extracted components form and listitem

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,13 @@ import { useState, useEffect } from "react";
 import Head from "next/head";
 import { Roboto } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import Form from "@/components/Form/Form";
+import ListItem from "@/components/ListItem/ListItem";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400"] });
 
 export default function Home() {
 	const [isSaving, setIsSaving] = useState(false);
-	const [task, setTask] = useState("");
 	const [allTasks, setAllTasks] = useState([]);
 
 	useEffect(() => {
@@ -28,27 +29,25 @@ export default function Home() {
 		e.preventDefault();
 		setIsSaving(true);
 		setAllTasks((current) => [...current, task]);
-		setTask("");
 	};
 	return (
 		<>
 			<Head>
 				<title>Ordino App</title>
-				<meta name="description" content="Devide and conquer your tasks" />
+				<meta name="description" content="Devide and conquer your tasks, microtask by microtask" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main className={styles.main} style={roboto.style}>
 				<h1>Ordino App</h1>
-				<form>
-					<input type="text" name="task" value={task} onChange={(e) => setTask(e.target.value)} />
-					<button type="submit" onClick={saveTask}>
-						save
-					</button>
-				</form>
+				<Form saveTask={saveTask} />
 				<ul>
 					{allTasks.map((task, index) => {
-						return <li key={index}>{task}</li>;
+						return (
+							<div key={index}>
+								<ListItem task={task} saveTask={saveTask} />
+							</div>
+						);
 					})}
 				</ul>
 			</main>
