7ae096d1a124ca35e92f5a72bab7c35908e9d898
Sergiu Plesco <sergiu.plesco@smartdata.solutions>
2023-03-13 16:47:02 +0200

    Issue #6: installed react-redux and commented handleBlur function

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,48 +2,48 @@ import React, { useState, useRef, useEffect } from "react";
 import styles from "@/components/BoardTitle/BoardTitle.module.css";
 
 const Form = ({ item, saveBoardTitle, isEditing, setIsEditing }) => {
-	const [value, setValue] = useState(item.title);
-	const inputRef = useRef(null);
+  const [value, setValue] = useState(item.title);
+  const inputRef = useRef(null);
 
-	const handleChange = (e) => {
-		setValue(e.target.value);
-	};
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		setIsEditing(false);
-		saveBoardTitle(item.id, value);
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsEditing(false);
+    saveBoardTitle(item.id, value);
+  };
 
-	const handleBlur = (e) => {
-		e.preventDefault();
-		setIsEditing(false);
-		saveBoardTitle(item.id, value);
-	};
+  const handleBlur = (e) => {
+    e.preventDefault();
+    setIsEditing(false);
+    saveBoardTitle(item.id, value);
+  };
 
-	useEffect(() => {
-		if (item.isNew) {
-			inputRef.current.focus();
-		}
-		if (isEditing) {
-			inputRef.current.focus();
-		}
-	}, [isEditing]);
+  useEffect(() => {
+    if (item.isNew) {
+      inputRef.current.focus();
+    }
+    if (isEditing) {
+      inputRef.current.focus();
+    }
+  }, [isEditing]);
 
-	return (
-		<>
-			<input
-				className={styles.noBorder}
-				ref={inputRef}
-				type="text"
-				name="task"
-				value={value}
-				onChange={handleChange}
-				onBlur={handleBlur}
-			/>
-			<button onClick={handleSubmit}>save</button>
-		</>
-	);
+  return (
+    <>
+      <input
+        className={styles.noBorder}
+        ref={inputRef}
+        type="text"
+        name="task"
+        value={value}
+        onChange={handleChange}
+        // onBlur={handleBlur}
+      />
+      <button onClick={handleSubmit}>save</button>
+    </>
+  );
 };
 
 export default Form;
